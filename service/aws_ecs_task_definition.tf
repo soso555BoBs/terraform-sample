516a0d46fadfdbf99d7936e4bd8cc137e3767d9a
soso555BoBs <soso555bobs@gmail.com>
2023-07-17 01:14:48 +0900

    add ecs

---
+++ service/aws_ecs_task_definition.tf
@@ -0,0 +1,7 @@
+resource "aws_ecs_task_definition" "sample-task" {
+    family = "sample-service"
+    container_definitions = file("./task_definitions/service.json")
+
+    task_role_arn = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+    network_mode = "bridge"
+}
